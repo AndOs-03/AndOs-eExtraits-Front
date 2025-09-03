bffa3fb6251f2c2ef582c92f0aed0121b34682cb
anderson.ouattara <anderson.ouattara@inexa-ci.com>
2025-09-03 08:31:59 +0000

    init: Configuration du template.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# AndOs-eExtraits-Front
-AndOs eExtraits est un logiciel de génération d'extraits d'États Civils (Naissances, Mariages,  Décès)
+# AndOs-eExtraits-APIs
+AndOs eExtraits est un logiciel de génération d'extraits d'États Civils (`Naissances, Mariages, 
+Décès`)
---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import AppLayout from "./layout/AppLayout";
+import { ScrollToTop } from "./components/common/ScrollToTop";
+import NotFound from "./pages/OtherPage/NotFound";
+import Home from "./pages/Dashboard/Home";
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          {/* Dashboard Layout */}
+          <Route element={<AppLayout />}>
+            <Route index path="/" element={<Home />} />
+            
+          </Route>
+
+          {/* Auth Layout */}
+
+          {/* Fallback Route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
