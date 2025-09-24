1b5ec6d7a4e3a7fc5e678876c390d12b690ec19c
anderson.ouattara <anderson.ouattara@inexa-ci.com>
2025-09-24 03:10:32 +0000

    refactor: Refactoring mineur.

--- src/services/extrait-naissance.service.ts
+++ src/services/extrait-naissance.service.ts
@@ -1,6 +1,6 @@
 import {ENDPOINTS} from "../api/endpoints";
 import {ApiError} from "../api/types.ts";
-import {apiExtraitsMariages} from "../api/apiExtraitsMariages.ts";
+import {apiMethodes} from "../api/apiMethodes.ts";
 import {
   ExtraitNaissanceEssentielVM
 } from "../models/ExtraitsNaissances/extrait-naissance-essentiel.model.ts";
@@ -15,29 +15,29 @@ import {
 } from "../pages/ExtraitsNaissances/modifier-extrait-naissance.commande.ts";
 
 export const listerExtraitsNaissances = async (centreId: number): Promise<ApiError | ExtraitNaissanceEssentielVM[]> => {
-  return await apiExtraitsMariages.get<ExtraitNaissanceEssentielVM[]>(ENDPOINTS.extraitsNaissances.list(centreId));
+  return await apiMethodes.get<ExtraitNaissanceEssentielVM[]>(ENDPOINTS.extraitsNaissances.list(centreId));
 };
 
 export const creerExtraitsNaissances = async (commande: CreerExtraitNaissanceCommande): Promise<ApiError | ExtraitNaissanceDetailsVM> => {
-  return await apiExtraitsMariages.post<ExtraitNaissanceDetailsVM>(ENDPOINTS.extraitsNaissances.create(), commande);
+  return await apiMethodes.post<ExtraitNaissanceDetailsVM>(ENDPOINTS.extraitsNaissances.create(), commande);
 };
 
 export const modifierExtraitsNaissances = async (commande: ModifierExtraitNaissanceCommande): Promise<ApiError | ExtraitNaissanceDetailsVM> => {
-  return await apiExtraitsMariages.put<ExtraitNaissanceDetailsVM>(ENDPOINTS.extraitsNaissances.update(), commande);
+  return await apiMethodes.put<ExtraitNaissanceDetailsVM>(ENDPOINTS.extraitsNaissances.update(), commande);
 };
 
 export const supprimerExtraitsNaissances = async (id: number): Promise<ApiError | ExtraitNaissanceDetailsVM> => {
-  return await apiExtraitsMariages.delete<ExtraitNaissanceDetailsVM>(ENDPOINTS.extraitsNaissances.delete(id));
+  return await apiMethodes.delete<ExtraitNaissanceDetailsVM>(ENDPOINTS.extraitsNaissances.delete(id));
 };
 
 export const recupererExtraitNaissances = async (id: number): Promise<ApiError | ExtraitNaissanceDetailsVM> => {
-  return await apiExtraitsMariages.get<ExtraitNaissanceDetailsVM>(ENDPOINTS.extraitsNaissances.get(id));
+  return await apiMethodes.get<ExtraitNaissanceDetailsVM>(ENDPOINTS.extraitsNaissances.get(id));
 };
 
 export const recupererExtraitNaissanceEssentiel = async (id: number): Promise<ApiError | ExtraitNaissanceEssentielVM> => {
-  return await apiExtraitsMariages.get<ExtraitNaissanceEssentielVM>(ENDPOINTS.extraitsNaissances.getEssentiel(id));
+  return await apiMethodes.get<ExtraitNaissanceEssentielVM>(ENDPOINTS.extraitsNaissances.getEssentiel(id));
 };
 
 export const recupererExtraitNaissancesParRegistre = async (centreId: number, registre: string): Promise<ApiError | ExtraitNaissanceDetailsVM> => {
-  return await apiExtraitsMariages.get<ExtraitNaissanceDetailsVM>(ENDPOINTS.extraitsNaissances.getByRegistre(centreId, registre));
+  return await apiMethodes.get<ExtraitNaissanceDetailsVM>(ENDPOINTS.extraitsNaissances.getByRegistre(centreId, registre));
 };
